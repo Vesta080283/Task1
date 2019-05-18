5b82613ee77b017822cfe828a6df0e5803e1afb6
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-18 11:27:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 'use strict'
-document.querySelector('#addBtn').addEventListener('click', () => {
+window.onload = () => {
+  document.querySelector('#addBtn').addEventListener('click', addMethod);
+  const addMethod = () => {
     let num1 = document.querySelector('#n1').value;
     let num2 = document.querySelector('#n2').value;
 
-    if (isNaN(num1) || num1 == '') {
-        num1 = 0.1;
-        document.querySelector('#n1').defaultValue = num1;
+    if( isNaN(num1) || num1=='') {
+      num1 = 0.1;
+      document.querySelector('#n1').defaultValue = num1;    
     }
 
-    if (isNaN(num2) || num2 == '') {
-        num2 = 0.2;
-        document.querySelector('#n2').defaultValue = num2;
+    if ( isNaN(num2) || num2=='' ) {
+      num2 = 0.2;
+      document.querySelector('#n2').defaultValue = num2;
     }
 
     num1 = Number(num1);
     num2 = Number(num2);
-    let result;
+    let result; 
     result = num1 + num2;
 
-    alert("Вывод: " + result);
-});
+    alert('Результат: ' + String(result));
+  }
+}

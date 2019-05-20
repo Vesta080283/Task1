41b88c0165b1f0063fa573c3f69bb2f31c7919db
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-20 08:39:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 'use strict'
-window.onload = () => {
-  document.querySelector('#addBtn').addEventListener('click', addMethod);
-  const addMethod = () => {
-    let num1 = document.querySelector('#n1').value;
-    let num2 = document.querySelector('#n2').value;
+window.onload = () => {   
+    const addMethod = () => {
+        let num1 = document.querySelector('#n1').value;
+        let num2 = document.querySelector('#n2').value;
 
-    if( isNaN(num1) || num1=='') {
-      num1 = 0.1;
-      document.querySelector('#n1').defaultValue = num1;    
+        if (isNaN(num1) || num1 == '') {
+            num1 = 0.1;
+            document.querySelector('#n1').defaultValue = num1;
+        }
+
+        if (isNaN(num2) || num2 == '') {
+            num2 = 0.2;
+            document.querySelector('#n2').defaultValue = num2;
+        }
+
+        num1 = Number(num1);
+        num2 = Number(num2);
+
+        //получим количество децимальный разрядов в введёных числах и выберем большее из них
+        let countDec1 = countDigits(num1);
+        let countDec2 = countDigits(num2);
+        let countDec = (countDec1 >= countDec2) ? countDec1 : countDec2;
+
+        let result;
+        result = (num1 + num2).toFixed(countDec);
+
+        alert('Результат: ' + String(result));
     }
 
-    if ( isNaN(num2) || num2=='' ) {
-      num2 = 0.2;
-      document.querySelector('#n2').defaultValue = num2;
-    }
+    const countDigits = (num) => {
+       return (num.toString().includes('.')) ? (num.toString().split('.').pop().length) : 0;
+    }    
 
-    num1 = Number(num1);
-    num2 = Number(num2);
-    let result; 
-    result = num1 + num2;
-
-    alert('Результат: ' + String(result));
-  }
+    document.querySelector('#addBtn').addEventListener('click', addMethod);
+    
 }
